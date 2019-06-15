365b5b3a4fc85650b954cd6b5322aae49730dd39
Javier Romero <romeropaolini@gmail.com>
2019-06-15 12:44:52 +0200

    Seting up async handling

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -6,6 +6,7 @@ import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
 import { Redirect } from "react-router-dom";
+import Spinner from "./../common/Spinner";
 
 class CoursesPage extends React.Component {
   state = {
@@ -32,6 +33,7 @@ class CoursesPage extends React.Component {
     return (
       <>
         {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
+        <Spinner />
         <h2>Courses</h2>
 
         <button
