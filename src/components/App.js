96fee1a6936a9eff6d7b8ecbf0062e75a7061606
Javier Romero <romeropaolini@gmail.com>
2019-06-16 11:44:01 +0200

    Asunc status and error handling

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,11 @@ import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
-import CoursesPage from "./courses/CoursesPage";
 import PageNotFound from "./PageNotFound";
+import CoursesPage from "./courses/CoursesPage";
 import ManageCoursePage from "./courses/ManageCoursePage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
         <Route path="/course" component={ManageCoursePage} />
         <Route component={PageNotFound} />
       </Switch>
+      <ToastContainer autoClose={3000} hideProgressBar />
     </div>
   );
 }
